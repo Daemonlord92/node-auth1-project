6b661b44e6c6585df9c3d278131c80b80209f213
Matthew Martin <zhintateo19924559@gmail.com>
2021-02-10 19:53:47 -0600

    Setup the server file with the sessions and middleware

---
+++ api/server.js
@@ -0,0 +1,44 @@
+const session = require('express-session');
+
+const knexSessionStore = require('connect-session-knex')(session);
+
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+
+const server = express();
+
+const sessionConfig = {
+	name: 'mjmsession',
+	secret: '7bfece61a4dbbc7b427355fd1499a851',
+	cookie: {
+		maxAge: 1000 * 60 * 60,
+		secure: false,
+		httpOnly: true
+	},
+	resave: false,
+	saveUninitialized: false,
+
+	store: new knexSessionStore({
+		knex: require("../data/dbConfig.js"),
+		tablename: "sessions",
+		sidfieldname: "sid",
+		createtable: true,
+		clearInterval: 1000 * 60 * 60
+	})
+}
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.use(session(sessionConfig));
+
+server.get("/", (req, res) => {
+	res.json({
+		api: "up"
+	});
+});
+
+module.exports = server;
